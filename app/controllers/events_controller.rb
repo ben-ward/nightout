971980752285c92e87024e978f6d50e814538378
Ben Ward <ben@mauilabs.com>
2015-10-24 12:03:43 -1000

    Adding event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,20 @@
 class EventsController < ApplicationController
+
   def show
     @event = Event.find(params[:id])
   end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to event_path(@event), notice: 'Event created'
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def event_params
+    params.permit(:name, :time, :deadline)
+  end
+
 end
