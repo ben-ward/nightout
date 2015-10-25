2c338bfba4637ec359e11e95aeb31f482393a24b
Ben Ward <ben@mauilabs.com>
2015-10-25 00:48:20 -1000

    Fix responsiveness

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,7 +11,7 @@ class VotesController < ApplicationController
       @twilio_client.messages.create(
         from: '+18084950075',
         to: '+18082805484',
-        body: "Nice! We have decided to go to #{@event.destinations.order(:vote_count).first.name} Call an Uber here. #{event_url(@event)}"
+        body: "Nice! We have decided to go to #{@event.destinations.order('vote_count DESC').first.name} Call an Uber here. #{event_url(@event)}"
       )
     end
     respond_to do |format|
